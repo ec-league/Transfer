ad905d43d9a8453f8188e088e71a21134bfc9ae3
EthanPark <byp5303628@hotmail.com>
2017-03-04 16:40:39 +0800

    Define the project access

---
+++ src/main/java/com/ecleague/Transfer.java
@@ -0,0 +1,18 @@
+package com.ecleague;
+
+/**
+ * Author: EthanPark <br/>
+ * Date: 2017/3/4<br/>
+ * Email: byp5303628@hotmail.com
+ */
+public class Transfer {
+
+   /**
+    * Project access, which read the args to transfer a .cs file to java file.
+    * 
+    * @param args
+    */
+   public static void main(String[] args) {
+
+   }
+}
