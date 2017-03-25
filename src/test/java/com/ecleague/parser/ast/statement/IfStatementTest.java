fd6f681d1cf7fe73de5ee23059e4814529b0727c
yun.li <yun.li@Ctrip.com>
2017-03-25 16:45:46 +0800

    annotation

--- src/test/java/com/ecleague/parser/ast/statement/IfStatementTest.java
+++ src/test/java/com/ecleague/parser/ast/statement/IfStatementTest.java
@@ -7,31 +7,23 @@ import org.junit.Test;
  * Created by yun.li on 2017/3/25.
  */
 public class IfStatementTest {
-    @Test
-    public void parse() throws Exception {
-        String sourceCode = "if (x==2)" +
-                "                {" +
-                "                    return 5;" +
-                "                }" +
-                "                else " +
-                "                    if (x==3)" +
-                "                {" +
-                "                    return 7;" +
-                "                }" +
-                "                    else if (x == 3)" +
-                "                    {" +
-                "                        return 11;" +
-                "                    }" +
-                "                    else" +
-                "                    {" +
-                "                        return 15;" +
-                "                    }";
+   @Test
+   public void parse() throws Exception {
+      String sourceCode = "if (x==2)" + "                {"
+            + "                    return 5;" + "                }"
+            + "                else " + "                    if (x==3)"
+            + "                {" + "                    return 7;"
+            + "                }" + "                    else if (x == 3)"
+            + "                    {" + "                        return 11;"
+            + "                    }" + "                    else"
+            + "                    {" + "                        return 15;"
+            + "                    }";
 
-        IfStatement statement = new IfStatement();
+      IfStatement statement = new IfStatement();
 
-        Assert.assertEquals(statement.parse(sourceCode), "");
+      Assert.assertEquals(statement.parse(sourceCode), "");
 
-        Assert.assertNotNull(statement.getIfExpression());
-    }
+      Assert.assertNotNull(statement.getIfExpression());
+   }
 
 }
